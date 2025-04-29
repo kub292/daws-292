08136c7b50017b8cbcc563a0269d1507954d97a0
kub292 <kbandari292@gmail.com>
2025-04-29 12:48:18 -0400

    special types script

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -9,5 +9,6 @@ echo "Home directory of current user: $HOME"
 echo "which user is running this script: $USER"
 echo "Hostname: $HOSTNAME"
 echo "Process ID of the current shell script: $$"
-sleep 60
+sleep 60 &
+echo " process ID of last backgroud command: $!"
 

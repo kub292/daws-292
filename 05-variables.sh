5d18cc93e98d755ad56dd2e430c5995a26a745bb
kub292 <kbandari292@gmail.com>
2025-04-28 23:19:24 -0400

    CONFIDENTAIL updates script

---
+++ 05-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo " Please enter name::"
+
+read USERNAME  # here Username is variable
+
+echo "Please enter Password::"
+
+read PASSWORD
+
+
+echo "Username is $USERNAME, Password is $PASSWORD"

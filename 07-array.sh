331e5aef002929b416edea80b16cc57cbcd4ebee
kub292 <kbandari292@gmail.com>
2025-04-29 11:37:07 -0400

    Arrays updates script

---
+++ 07-array.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+COURSES=("Python" "PL/SQL" "Selenium")
+
+echo "First values is :: ${COURSES[0]}"
+echo "Second value is :: ${COURSES[1]}"
+echo "ALl values in my first array:: ${COURSES[@]}"

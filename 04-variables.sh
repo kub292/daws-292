eea024027d70a8ea093d060cec8e9bb0591a70bb
kub292 <kbandari292@gmail.com>
2025-04-28 23:10:46 -0400

    arguments updates script

---
+++ 04-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Hello world"
+
+# declaring of variables
+PERSON1=$1 # no space betwen = and value
+PERSON2=$2 
+
+# referring variables
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine. How are you"
+echo "$PERSON1:: I am fine too. how is your work?"
+echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps" 

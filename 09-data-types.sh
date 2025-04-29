89416bccb2b502eba420202b5ec2327f0af0afdb
kub292 <kbandari292@gmail.com>
2025-04-29 12:40:01 -0400

    specialypes script

---
+++ 09-data-types.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#special variable / interview questions 
+echo " ALL variable: $@"
+echo "Number of variables passed: $#"
+echo "Script Name: $0"
+echo "Current working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "which user is running this script: $USER"
+echo "Hostname: $HOSTNAME"
+echo "Process ID of the current shell script: $$"
+
+
